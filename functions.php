de9b762bde96aec3ed304222b230d97f4670b3ec
Amit Singh <amit@wpoets.com>
2026-05-27 23:18:47 +0530

    version updates

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 /**
  * Define theme constants
  */
-define('MONOMYTH_VERSION', '1.0.0');
+define('MONOMYTH_VERSION', '1.0.1');
 define('MONOMYTH_DIR', get_template_directory());
 define('MONOMYTH_URI', get_template_directory_uri());
 
